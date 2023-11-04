a835f4599d38a024d155631efe46a8ca4e4f3d6d
nhatlinh <nhatlinhdev201@gmail.com>
2023-11-04 08:31:25 +0700

    initial java Spring boot app for lab 07

---
+++ src/main/java/vn/edu/iuh/fit/WwwLab07Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwLab07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwLab07Application.class, args);
+	}
+
+}
